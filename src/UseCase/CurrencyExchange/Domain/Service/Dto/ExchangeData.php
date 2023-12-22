2979f63bd606026235e562d11099b00a00f21501
karolina <karolina.stanik000@gmail.com>
2023-12-22 17:10:00 +0100

    small changes

--- src/UseCase/CurrencyExchange/Domain/Service/Dto/ExchangeData.php
+++ src/UseCase/CurrencyExchange/Domain/Service/Dto/ExchangeData.php
@@ -4,11 +4,11 @@ namespace App\UseCase\CurrencyExchange\Domain\Service\Dto;
 
 use App\UseCase\CurrencyExchange\Domain\Entity\Currency;
 
-class ExchangeData
+readonly class ExchangeData
 {
-  private readonly Currency $from;
-  private readonly Currency $to;
-  private readonly float $amount;
+  private Currency $from;
+  private Currency $to;
+  private float $amount;
 
     public function __construct(Currency $from, Currency $to, float $amount)
     {
